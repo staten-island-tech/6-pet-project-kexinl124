f604b8aa26082be86486d6134b377277482495b7
kexinl124 <KExinl10@nycstudents.net>
2025-11-18 08:42:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 class pet:
-    def __init__(self, name, money, inventory,emotion):
+    def __init__(self, name, money, inventory,emotion,food):
         self.name = name
         self.money = money
         self.inventory = inventory
         self.emotion = emotion
+        self.food = food
 
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 
+    def eat(self,):
+        self.
+
 Coke = pet("Coke", 150, ["Potion"],"Happy")
 
 Coke.buy({"title": "Sword", "attack": 10000000000})
 print(Coke.__dict__)
+
+
 
 
 
