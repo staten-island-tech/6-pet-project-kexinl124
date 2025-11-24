3660e9b5a14934ddd538ac64d1aceca78c92ef28
kexinl124 <KExinl10@nycstudents.net>
2025-11-24 09:25:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
-class pet:
-    def __init__(self, name, money, inventory,emotion,food):
+class Pet:
+    def __init__(self, name, money, inventory, feeling):
         self.name = name
         self.money = money
         self.inventory = inventory
-        self.emotion = emotion
-        self.food = food
-
+        self.feeling = feeling
+        
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
-
-    def eat(self,item):
-        self.inventory.append(item)
-        print(self.inventory)
     
-    def hunger(self,item):
+    def feel(self,item):
         self.inventory.append(item)
         print(self.inventory)
 
-Coke = pet("Coke", 150, ["Potion"],"Happy","cookie")
+Coke = Pet("Coke", 150, ["Potion"], "netrual = 50%")
+Coke.buy({"cookie"})
+Coke.feel({"happy = 100%"})
 
-Coke.buy({"title": "Sword", "attack": 10000000000})
-print(Coke.__dict__)
 
-Coke.eat({"title": "Cookie"})
-print(Coke.__dict__)
 
-Coke.hunger({"percent":"10%"})
-print(Coke.__dict__)
+# class pet:
+#     def __init__(self, name, money, inventory,hungry):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
+#         self.hungry = hungry 
+
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+
+#     def eat(self,item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+    
+#     def hunger(self,item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+
+# Coke = pet("Coke", 150, ["Potion"],"netrual")
+
+# Coke.buy({"title": "Sword", "attack": 10000000000})
+
+# Coke.eat({"title": "Cookie"})
+# emotion = "happy"
+
+# Coke.hunger({"percent":"10%"})
+# print(Coke.__dict__)
 
 # class pet:
 #     def __init__(self, name, atk):
